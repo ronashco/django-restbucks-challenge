55b6eec7dd612320fb76e23207ed67fdc5b67c6a
Mohsen Parsaeian <goodartimes@gmail.com>
2017-07-06 18:35:13 +0430

    migrate to mysql

--- my_coffee/my_coffee/settings/local.py
+++ my_coffee/my_coffee/settings/local.py
@@ -11,8 +11,12 @@ DEBUG = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'my_coffee',
+        'USER': 'root',
+        'PASSWORD': '13731224',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -22,7 +26,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tehran'
 
 USE_I18N = True
 
