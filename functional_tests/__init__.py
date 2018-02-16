97c9cb5d2e93f3f52c9faf175b79102de39b32f5
Mohammad <mohammadho3eini73@gmail.com>
2018-02-16 17:18:34 +0330

    Add menu functional tests.

---
+++ functional_tests/__init__.py
@@ -0,0 +1,5 @@
+"""
+Functional (acceptance) tests are set of test cases based on the user story,
+in other words, we want to make sure our app works well from user's perspective.
+It's not about backend stuff.e.g. What database query does execute, Which template rendered and so on.
+"""
---
+++ functional_tests/test_product_list.py
@@ -0,0 +1,13 @@
+from django.test import LiveServerTestCase
+
+
+class TestProductList(LiveServerTestCase):
+    """
+    Make sure our menu works well.
+    """
+    def test__contains_primary_data(self):
+        response = self.client.get('/api/products/')
+        for p in response.json():
+            self.assertEqual(
+                {'title', 'price', 'options'}, set(p.keys())
+            )
