2384ee76c428c6a6b12d458c4fc385f32752069b
Mohammad <mohammadho3eini73@gmail.com>
2018-02-16 17:54:28 +0330

    Add product app.

--- config/settings/base.py
+++ config/settings/base.py
@@ -14,7 +14,9 @@ DJANGO_APPS = [
     'django.contrib.staticfiles',
 ]
 THIRD_PARTY_APPS = []
-PROJECT_APPS = []
+PROJECT_APPS = [
+    'core.products'
+]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + PROJECT_APPS
 
