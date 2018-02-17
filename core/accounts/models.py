e14698a5182136b65570e6b1533eada0346913bc
Mohammad <mohammadho3eini73@gmail.com>
2018-02-17 19:36:38 +0330

    Prepare backend stuff to authenticate users.

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,3 +1,24 @@
-from django.db import models
+from django.dispatch import receiver
+from django.contrib.auth import get_user_model
+from django.db.models.signals import pre_save, post_save
+from rest_framework.authtoken.models import Token
 
-# Create your models here.
+User = get_user_model()
+
+
+@receiver(pre_save, sender=User)
+def set_username(sender, instance=None, **kwargs):
+    """
+    For the sake of simplicity, We want to consider username as same as email,
+    in every CREATE‌ operation with ORM, we set the username automatically.
+    """
+    instance.username = instance.email
+
+
+@receiver(post_save, sender=User)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    """
+    Create a login token for every single user who created with ORM.
+    """
+    if created:
+        Token.objects.create(user=instance)
--- core/accounts/tests.py
+++ core/accounts/tests.py
@@ -1,2 +1,51 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+from .backends import EmailBackend
 
+User = get_user_model()
+
+
+class TestSignals(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        user = User(email='foo@email.com')
+        user.set_password('Aab123445678')
+        user.save()
+        cls.user = user
+
+    def test_token_generated(self):
+        """Make sure Token object created"""
+        self.assertTrue(
+            Token.objects.filter(user=self.user).exists()
+        )
+
+    def test_username_sets(self):
+        """Make sure username sets automatically as same as email."""
+        self.assertEqual(
+            self.user.email, self.user.username
+        )
+
+
+class TestEmailBackend(TestCase):
+    def setUp(self):
+        self.backend = EmailBackend()
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user_data = {
+            'email': 'm@email.com',
+            'password': 'Abc123456789'
+        }
+        user = User(email=cls.user_data['email'])
+        user.set_password(cls.user_data['password'])
+        user.save()
+        cls.user = user
+
+    def test_authenticate(self):
+        """Make sure backend can recognize users with their email address."""
+        user = self.backend.authenticate(username=self.user_data['email'],
+                                         password=self.user_data['password'])
+        self.assertEqual(
+            self.user, user
+        )
