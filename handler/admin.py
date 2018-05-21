8e859cee5944ddd950de3fa85155281aafa2beb7
pusheinterview <39477679+pusheinterview@users.noreply.github.com>
2018-05-22 01:38:13 +0430

    models created!

--- handler/admin.py
+++ handler/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Product , OrderStatus , Order
 # Register your models here.
+
+admin.site.register(Product)
+admin.site.register(OrderStatus)
+admin.site.register(Order)
--- handler/models.py
+++ handler/models.py
@@ -1,5 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
 
+class OrderStatus(models.Model):
+    status = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.status
+
+
+class Product(models.Model):
+    product = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.product
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User)
+    product = models.ForeignKey(Product)
+    consume_place = models.CharField(max_length=30)
+    option = models.CharField(max_length=30)
+    status = models.ForeignKey(OrderStatus)
+
+    def __str__(self):
+        return self.id
