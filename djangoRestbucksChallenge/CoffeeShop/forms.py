6ccccc9921a5a98f31d3f815b0ac43447734b35f
parishadbehnam <pbehnamghader@yahoo.com>
2018-05-23 08:35:27 +0430

    LoginForm for rendering and submitting a login request

---
+++ djangoRestbucksChallenge/CoffeeShop/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from django.forms import ModelForm
+from djangoRestbucksChallenge.CoffeeShop import models
+
+from django.contrib.auth import authenticate
+
+
+class LoginForm(ModelForm):
+
+    class Meta:
+        model = models.Customer
+        fields = ['username', 'password']
+        widgets = {
+            'password': forms.TextInput(attrs={'type': 'password'})
+        }
+
+    def clean(self):
+        cleaned_data = super(LoginForm, self).clean()
+        username = cleaned_data["username"]
+        password = cleaned_data["password"]
+        user = authenticate(self.data, username=username, password=password)
+
+        if user is None:
+            raise forms.ValidationError("Error")
+
+        return cleaned_data
