68098b1e25476d876359d2349381ba4a3fa8bc14
parishadbehnam <pbehnamghader@yahoo.com>
2018-05-23 12:25:55 +0430

    orderline and order models created

--- djangoRestbucksChallenge/OrderManagement/models.py
+++ djangoRestbucksChallenge/OrderManagement/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
-
+from CoffeeShop.models import Customer
 from django.db import models
+from datetime import datetime
 
 
 class Product(models.Model):
@@ -20,3 +21,17 @@ class CustomizedProduct(models.Model):
         return str(self.product) + ' --> ' + self.option + ' : ' + self.type
 
 
+class Order(models.Model):
+    status = models.CharField(choices=(('waiting', 'waiting'), ('preparation', 'preparation'), ('ready', 'ready'),
+                                       ('delivered', 'delivered')), max_length=20, default='waiting')
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    location = models.CharField(choices=(('coffeeshop', 'inshop'), ('home', 'takeaway')), max_length=10)
+    order_time = models.DateTimeField(default=datetime.now)
+
+    def __str__(self):
+        return str(self.pk)
+
+
+class OrderLine(models.Model):
+    customized_product = models.ForeignKey(CustomizedProduct, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
