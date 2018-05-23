396e9e45cdadccb5b765616f468abb65c17cd008
parishadbehnam <pbehnamghader@yahoo.com>
2018-05-23 16:20:16 +0430

    customer can see an order

--- djangoRestbucksChallenge/OrderManagement/views.py
+++ djangoRestbucksChallenge/OrderManagement/views.py
@@ -53,3 +53,20 @@ def view_orders(request):
     orders = Order.objects.filter(customer=request.user)
     return render(request, 'view_orders.html', {'orders': orders})
 
+
+def get_order_details(id):
+    order = Order.objects.get(pk=id)
+    price = OrderLine.objects.filter(order=order).aggregate(Sum('customized_product__product__price'))
+    price = price['customized_product__product__price__sum']
+    return order, price
+
+
+@login_required
+def view_an_order(request):
+    order, price = get_order_details(request.GET.get('id'))
+    products = Product.objects.all()
+
+    my_customized_products_id = [orderline.customized_product.id for orderline in order.orderline_set.all()]
+    return render(request, 'view_an_order.html', {'order': order, 'price': price, 'products': products,
+                                                  'my_customized_products_id' : my_customized_products_id})
+
