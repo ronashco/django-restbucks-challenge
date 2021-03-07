d11fa162e717bb3e96d4eaee17baa8461efdab6e
Behnam <bha.1370@yahoo.com>
2021-03-07 12:44:59 +0330

    clean | url | none existing views

--- django_restbucks_challenge/urls.py
+++ django_restbucks_challenge/urls.py
@@ -6,7 +6,4 @@ from restbuck_app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('menu/', views.Menu.as_view()),
-    path('test/', views.Features.as_view()),
-    path('myorders/', views.MyOrders.as_view()),
-    path('myorders/<int:pk>/', views.MyOrder.as_view()),
     ]
