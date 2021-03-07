d078abffccfeafe450b656d6aabcd72e12758843
Behnam <bha.1370@yahoo.com>
2021-03-07 12:28:19 +0330

    models initial

--- restbuck_app/models.py
+++ restbuck_app/models.py
@@ -1,1 +1,95 @@
 from django.db import models
+
+
+class ConsumeLocation(models.Model):
+    take_away = 0
+    in_shop = 1
+    types = (
+        (take_away, 'take away'),
+        (in_shop, 'in shop'),
+    )
+
+
+class OrderStatus(models.Model):
+    waiting = 0
+    preparation = 1
+    ready = 2
+    delivered = 3
+    types = (
+        (waiting, 'waiting'),
+        (preparation, 'preparation'),
+        (ready, 'ready'),
+        (delivered, 'delivered'),
+    )
+
+
+class Feature(models.Model):
+    title = models.CharField(max_length=255)
+    # TODO: change to 'label' and 'code' to support multiple 'kind' for different product
+
+    def __str__(self):
+        return self.title
+
+    def get_values(self):
+        return ', '.join([y.title for y in FeaturesValue.objects.filter(feature=self)])
+
+
+class FeaturesValue(models.Model):
+    feature = models.ForeignKey(Feature, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.feature.title+'-->'+self.title
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    cost = models.IntegerField()
+    feature_list = models.ManyToManyField(Feature, through='ProductFeature')
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def feature_lists(self):
+        return ', '.join([x.title for x in self.feature_list.all()])
+
+    @property
+    def feature_list_with_values(self):
+        return ', '.join([x.title+': '+x.get_values() for x in self.feature_list.all()])
+
+
+class ProductFeature(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    feature = models.ForeignKey(Feature, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.product.title+' '+self.feature.title
+
+
+class ProductOrderFeatureValue(models.Model):
+    product_order = models.ForeignKey('ProductOrder', on_delete=models.CASCADE)
+    feature_List = models.ForeignKey(FeaturesValue, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.id
+
+
+class ProductOrder(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey('Order', on_delete=models.CASCADE)
+    count = models.IntegerField()
+    consume_location = models.SmallIntegerField(choices=ConsumeLocation.types)
+    feature_value_list = models.ManyToManyField(FeaturesValue, through=ProductOrderFeatureValue)
+
+    def __str__(self):
+        return self.count.__str__()+'*'+self.product.title+'--orderNo: '+self.order.id.__str__()
+
+
+class Order(models.Model):
+    status = models.SmallIntegerField(choices=OrderStatus.types, default=OrderStatus.waiting)
+    product_list = models.ManyToManyField(Product, through=ProductOrder, related_name='products')
+
+    def __str__(self):
+        return 'id:'+self.id.__str__()+'-'+', '.join([x.title for x in self.product_list.all()])
+
