eefa12377f7fdbbf2e7d215ac43933b206a1fe14
Behnam <bha.1370@yahoo.com>
2021-03-07 12:11:31 +0330

    app structure

---
+++ restbuck_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RestbuckAppConfig(AppConfig):
+    name = 'restbuck_app'
---
+++ restbuck_app/tests.py
@@ -0,0 +1,4 @@
+from django.test import TestCase
+from restbuck_app.models import *
+
+
