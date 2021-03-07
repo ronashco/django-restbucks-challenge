5f623d6c63076d62dab33f50a7903165569c7b9c
Behnam <bha.1370@yahoo.com>
2021-03-07 12:31:14 +0330

    url init

--- django_restbucks_challenge/urls.py
+++ django_restbucks_challenge/urls.py
@@ -1,1 +1,12 @@
 from django.contrib import admin
+from django.urls import path
+
+from restbuck_app import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('menu/', views.Menu.as_view()),
+    path('test/', views.Features.as_view()),
+    path('myorders/', views.MyOrders.as_view()),
+    path('myorders/<int:pk>/', views.MyOrder.as_view()),
+    ]
