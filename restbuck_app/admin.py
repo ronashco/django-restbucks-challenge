4616e4de3cf8ec66b73c2afcdba6164d58897772
Behnam <bha.1370@yahoo.com>
2021-03-07 12:30:15 +0330

    admin init

---
+++ restbuck_app/admin.py
@@ -0,0 +1,69 @@
+from django.contrib import admin
+from restbuck_app.models import *
+
+
+class FeatureAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in Feature._meta.fields]
+
+
+admin.site.register(Feature, FeatureAdmin)
+
+
+class FeaturesValueAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in FeaturesValue._meta.fields]
+
+
+admin.site.register(FeaturesValue, FeaturesValueAdmin)
+
+
+class ProductFeatureAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in ProductFeature._meta.fields]
+
+
+admin.site.register(ProductFeature, ProductFeatureAdmin)
+
+
+class ProductFeatureInline(admin.TabularInline):
+    model = ProductFeature
+    extra = 1
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in Product._meta.fields]
+    inlines = (ProductFeatureInline,)
+
+
+admin.site.register(Product, ProductAdmin)
+
+
+class ProductOrderInline(admin.TabularInline):
+    model = ProductOrder
+    extra = 1
+
+
+class ProductOrderFeatureValueAdmin(admin.TabularInline):
+    model = ProductOrderFeatureValue
+    extra = 1
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in Order._meta.fields]
+    inlines = (ProductOrderInline,)
+
+
+admin.site.register(Order, OrderAdmin)
+
+
+class ProductOrderAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in ProductOrder._meta.fields]
+    inlines = (ProductOrderFeatureValueAdmin,)
+
+
+admin.site.register(ProductOrder, ProductOrderAdmin)
+
+
+class ProductOrderFeatureValueAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in ProductOrderFeatureValue._meta.fields]
+
+
+admin.site.register(ProductOrderFeatureValue, ProductOrderFeatureValueAdmin)
