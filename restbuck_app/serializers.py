d6a8d4c37ba4dc96fea7e8185743492273229d6b
Behnam <bha.1370@yahoo.com>
2021-03-07 14:14:30 +0330

    update | FeatureWithValuesSerializer | value list of feature

--- restbuck_app/serializers.py
+++ restbuck_app/serializers.py
@@ -15,6 +15,21 @@ class FeatureSerializer(serializers.ModelSerializer):
         fields = ('title',)
 
 
+class FeatureWithValuesSerializer(serializers.ModelSerializer):
+    value_list = SerializerMethodField(read_only=True)
+    # value_list = FeatureValueSerializer(many=True, read_only=True)
+    # value_list = FeatureSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Feature
+        fields = ('title', 'value_list')
+
+    def get_value_list(self, obj):
+        values = obj.featuresvalue_set.all()
+        serialized_fv = FeatureValueSerializer(values, many=True, read_only=True).data
+        return serialized_fv
+
+
 class ProductSerializer(serializers.ModelSerializer):
     feature_list = FeatureWithValuesSerializer(read_only=True, many=True)
 
