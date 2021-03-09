282ca8a84f1ae51c2d11c3a9f72bf19b807e911a
Behnam <bha.1370@yahoo.com>
2021-03-09 21:10:12 +0330

    add | OrderView.post as patch :D

--- restbuck_app/views.py
+++ restbuck_app/views.py
@@ -48,6 +48,7 @@ class OrderView(APIView):
             orders = Order.objects.filter(user=user).exclude(status=OrderStatus.canceled)
             for order in orders:
                 data.append(OrderSerializer(order).data)
+        # TODO: check for empty product list
         return Response({'data': data,
                          'error': False})
 
@@ -71,15 +72,26 @@ class OrderView(APIView):
 
     def post(self, request, pk=0):
         user = get_auth_user(request)
-        order = Order.objects.create(user=user)
+        tobe_deleted = None
+        if pk > 0:
+            order, response_status = self.get_object(pk, user)
+            tobe_deleted = ProductOrder.objects.filter(order=order)
+            if response_status == status.HTTP_404_NOT_FOUND:
+                return Response({'error': True, 'message': 'requested order dose not exist'}, response_status)
+            elif response_status == status.HTTP_403_FORBIDDEN:
+                return Response({'error': True, 'message': 'Not your order'}, response_status)
+        elif pk == 0:
+            order = Order.objects.create(user=user)
+        else:
+            return Response({'error': True, 'message': 'Not valid order id'}, status.HTTP_400_BAD_REQUEST)
+
         data = request.data.get('data')
         serializer = ProductOrderFlatSerializer(data=data, many=True)
         if serializer.is_valid():
+            # TODO: must be changed for production, specially without log, it is dangerous
+            if tobe_deleted is not None:
+                tobe_deleted.delete()
             serializer.save(order=order)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-
-
