7b08cabd6527b08a62b30de8cac82c61e5f85ec7
Behnam <bha.1370@yahoo.com>
2021-03-09 13:40:36 +0330

    update | OrderView serve single order by id and all user's order

--- restbuck_app/views.py
+++ restbuck_app/views.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.http import Http404
@@ -19,12 +20,24 @@ def get_auth_user(request):
     return User.objects.get(id=1)
 
 
-class MyOrders(APIView):
-    def get(self, request):
+class OrderView(APIView):
+    def get_object(self, pk):
+        try:
+            return Order.objects.get(pk=pk)
+        except Order.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk=0):
         user = get_auth_user(request)
-        orders = Order.objects.filter(user=user)
-        data = []
-        for order in orders:
-            data.append(OrderSerializer(order).data)
+        if pk != 0:
+            order = self.get_object(pk)
+            if order.user != user:
+                return Response({'error': True, 'message': 'not your order'}, status=status.HTTP_403_FORBIDDEN)
+            data = OrderSerializer(order).data
+        else:
+            orders = Order.objects.filter(user=user)
+            data = []
+            for order in orders:
+                data.append(OrderSerializer(order).data)
         return Response({'data': data,
                          'error': False})
