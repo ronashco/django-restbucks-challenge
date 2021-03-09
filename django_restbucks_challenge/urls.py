67ba528cc024dce3e288815bcf9708524969c9b0
Behnam <bha.1370@yahoo.com>
2021-03-09 13:39:20 +0330

    update | order url, integrate user order request

--- django_restbucks_challenge/urls.py
+++ django_restbucks_challenge/urls.py
@@ -6,5 +6,6 @@ from restbuck_app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('menu/', views.Menu.as_view()),
-    path('myorders/', views.MyOrders.as_view()),
+    path('client_order/', views.OrderView.as_view()),
+    path('client_order/<int:pk>/', views.OrderView.as_view()),
     ]
