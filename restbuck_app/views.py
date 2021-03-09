2bd11452adf609760365c881cf46cbbccb0e67c3
Behnam <bha.1370@yahoo.com>
2021-03-09 11:26:33 +0330

    add | MyOrder API view and get_auth_user for dev

--- restbuck_app/views.py
+++ restbuck_app/views.py
@@ -13,3 +13,18 @@ class Menu(APIView):
             data.append(ProductSerializer(product).data)
         return Response({'data': data,
                          'error': False})
+
+
+def get_auth_user(request):
+    return User.objects.get(id=1)
+
+
+class MyOrders(APIView):
+    def get(self, request):
+        user = get_auth_user(request)
+        orders = Order.objects.filter(user=user)
+        data = []
+        for order in orders:
+            data.append(OrderSerializer(order).data)
+        return Response({'data': data,
+                         'error': False})
